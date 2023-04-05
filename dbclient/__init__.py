3c4a17d10a580ecd33f2727eec4f53c386601979
Rohan Gupta <simplyrohan10@gmail.com>
2023-04-05 12:32:04 -0700

    File class rewrite

--- dbclient/__init__.py
+++ dbclient/__init__.py
@@ -5,27 +5,62 @@ A simple but pwerful way to manage local databases in Python!
 
 import os
 import threading
+import json
+
+lock = threading.Lock()
 
 
 class Directory:
-    def __init__(self, directory) -> None:
-        self.dir = directory
-        self.lock = threading.Lock()
+    def __init__(self, path) -> None:
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
 
-    def __getitem__(self, target):
+        self.path = path
 
-        with self.lock:
-            slice_type = type(target)
-            if slice_type == str:
-                path = os.path.join(self.dir, target)
-                if os.path.isdir(path):
-                    return Directory(path)
-                else:
-                    return Data(path)
-    
-    def __repr__(self) -> str:
-        return f"<Directory {self.dir}>"
+    def __getitem__(self, item):
+        # Determine of item is a file or directory
+        all_items = os.listdir(self.path)
+        all_files = [
+            name.strip(".json") for name in all_items if name.endswith(".json")
+        ]
 
-class Data:
+        # TODO: The system of checking if the target is a file is a rushed and cheeky
+        # It's purpose it to allow users to enter the entry name with out having to add the .json suffix
+
+        if item in all_files:
+            return File(os.path.join(self.path, item.strip(".json") + ".json"))
+        else:
+            return Directory(os.path.join(self.path, item))
+
+    def __setitem__(self, item, value: dict):
+        # Only to be used on files
+        with lock:
+            all_folders = [
+                folder
+                for folder in os.listdir(self.path)
+                if os.path.isdir(os.path.join(self.path, folder))
+            ]
+            if item not in all_folders:
+                with open(
+                    os.path.join(self.path, item.strip(".json") + ".json"), "w"
+                ) as file:
+                    json.dump(value, file)
+
+    def __repr__(self):
+        return self.path + " Quick Look: \n\t" + "\n\t".join(os.listdir(self.path))
+
+
+class File(dict):
     def __init__(self, path) -> None:
         self.path = path
+
+        with lock:
+            with open(self.path, "r") as file:
+                super().__init__(json.load(file))
+
+    def push(self):
+        with lock:
+            with open(self.path, "w") as file:
+                json.dump(super().copy(), file, indent=4)
